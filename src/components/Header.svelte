e22945a73cfc9543d9b9cf410b8a37d70fe08534
yzaolavides <mpolavides1@up.edu.ph>
2026-04-28 16:36:19 +0400

    source pathing fix

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,17 +2,17 @@
     let is_navbar_closed = $state(true);
 	function dropMenu(){is_navbar_closed = !is_navbar_closed}
 </script>
-        <div class="navbar">
-			<div class="content">
-				<div style="display: flex; padding: 1rem 0 1rem 0;">
-					<a href="#" style="text-decoration: none"><span class="logo">yzabel olavides</span></a>
-					<button class="dropdown" onclick={dropMenu}><i class="fa-solid fa-bars"></i></button>
-				</div>
-				<div class="links" class:closed={is_navbar_closed}>
-					<a href="#works" style="text-decoration: none"><p>Works</p></a>
-					<a href="#about" style="text-decoration: none"><p>About</p></a>
-					<a href="https://drive.google.com/file/d/1XyeUJm4gusBrfuKa15-ZYyr8c0rrKa90/view?usp=sharing" style="text-decoration: none" target="_blank"><p>Resume</p></a>
-					<a href="https://vimeo.com/yzaolavides" style="text-decoration: none" target="_blank"><p>Videos</p></a>
-				</div>
+    <div class="navbar">
+		<div class="content">
+			<div style="display: flex; padding: 1rem 0 1rem 0;">
+				<a href="#" style="text-decoration: none"><span class="logo">yzabel olavides</span></a>
+				<button class="dropdown" onclick={dropMenu}><i class="fa-solid fa-bars"></i></button>
+			</div>
+			<div class="links" class:closed={is_navbar_closed}>
+				<a href="#works" style="text-decoration: none"><p>Works</p></a>
+				<a href="#about" style="text-decoration: none"><p>About</p></a>
+				<a href="https://drive.google.com/file/d/1XyeUJm4gusBrfuKa15-ZYyr8c0rrKa90/view?usp=sharing" style="text-decoration: none" target="_blank"><p>Resume</p></a>
+				<a href="https://vimeo.com/yzaolavides" style="text-decoration: none" target="_blank"><p>Videos</p></a>
 			</div>
 		</div>
+	</div>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({ 
+    plugins: [tailwindcss(), sveltekit()] ,
+    resolve: {
+        alias: {
+            src: path.resolve('src/'),
+        },
+    }
+});
+
+// Source - https://stackoverflow.com/a/73754976
+// Posted by brunnerh, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-28, License - CC BY-SA 4.0
+
+// ...
